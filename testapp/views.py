778f1578d3c15fe1da3ae03434ad296c82e5e646
Sajjad Ali <yoursajju61@gmail.com>
2026-06-28 17:15:41 +0530

    feat: add def post for post Response

--- testapp/views.py
+++ testapp/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from testapp.serializers import NameSerializer
 
 # Create your views / business logic here. 👇
 
 class TestAPIView(APIView):  # TestAPIView is a child class of APIView
+    # GET
     def get(self, request, *args, **kwargs):
         coding = ['python', 'java', 'C#', 'javaScript', 'C++']
 
         # Response class is responsible to convert python dict to json_data
         return Response({'msg':'Happy Coding', 'coding':coding})
+
+
+    # POST
+    def post(self, request, *args, **kwargs):
+        serializer =  NameSerializer(data = request.data)
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            msg = f'Hey {name}, Hapy ending of DRF classes'
+            return Response({'msg':msg})
+        else:
+            return Response(serializer.errors, status=400)
+
