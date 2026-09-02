078ddb6fd02a97fd821dc8b20d913886e31d583b
Sajjad Ali <yoursajju61@gmail.com>
2026-09-02 15:35:27 +0530

    API ViewSet

--- testapp/views.py
+++ testapp/views.py
@@ -39,18 +39,29 @@ class TestAPIView(APIView):  # TestAPIView is a child class of APIView
 
 
 class TestViewSet(ViewSet):
-    def list(self, request):
+
+    # List 
+    def list(self, request):  # # To Get all records 
         colours = ['red', 'green', 'blue']
         return Response({'msg':'Learning DRF', 'colours':colours})
 
+    #  # Create
+    # def create(self, request, pk=None):  # To Get a New Record
+    #     return Response({'msg':'This is from CREATE method of ViewSet'})
 
-    def retrive(self, request, pk=None):
+    # # Update
+    # def update(self, request, pk=None):  # To Update a Record
+    #     return Response({'msg':'This is from UPDATE method of ViewSet'})
+
+    # Retrive
+    def retrive(self, request, pk=None):  # To Get Single Record
         return Response({'msg':'This is from RETRIVE method'})
 
-    def partial_update(self, request, pk=None):
+    # Partial Update
+    def partial_update(self, request, pk=None):  # To Update few fields of data
         return Response({'msg':'This is from PARTIAL UPDATE method'})
         
-
-    def destroy(self, request, pk=None):
+    # Destroy
+    def destroy(self, request, pk=None):  # # To Delete a Record
         return Response({'msg':'This is from DESTROY method'})
         
